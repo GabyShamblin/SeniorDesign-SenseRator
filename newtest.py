46da23c888ce338570651dd59c0ccdee3a3bc74e
GabyShamblin <gabyshamblin@gmail.com>
2023-10-25 12:05:56 -0400

    Added point cloud updating

--- newtest.py
+++ newtest.py
@@ -1,25 +1,31 @@
-import copy
-import matplotlib.pyplot as plt
-import numpy as np
-import keyboard
+import os
+import time
 import open3d as o3d
 import open3d.visualization.gui as gui
 import PySimpleGUI as sui
 import convertLidar
 # import vlc
 
-print("Use SimpleGUI with point cloud window")
+print("Animated point cloud test")
 
 app = o3d.visualization.gui.Application.instance
 app.initialize()
 
-# sample_ply_data = o3d.data.PLYPointCloud()
-# pcd = o3d.io.read_point_cloud(sample_ply_data.path)
+path = ".\Data\pcd_files"
 
-hotel_cloud = o3d.io.read_point_cloud("Hotel.ply")
-car_file = convertLidar.pcap_to_ply("Data\car_lidar_test.pcap", "Data\car_lidar_metadata.json", ply_dir="Data\.")
-car_cloud = o3d.io.read_point_cloud("Data\car_lidar_test.pcap")
+# Unpack point clouds to seperate files
+# try:
+# 	convertLidar.pcap_to_pcd("Data\car_lidar_test.pcap", "Data\car_lidar_metadata.json", pcd_dir=f"{path}\.")
+# except Exception as e:
+# 	print(e)
+
+# point_cloud = o3d.io.read_point_cloud(f"{path}\pcd_out_000000.pcd")
+point_cloud = o3d.geometry.PointCloud()
+point_cloud_name = "Scene"
+# hotel_cloud = o3d.io.read_point_cloud("Hotel.ply")
 # player = vlc.MediaPlayer()
+
+# ----- SimpleUI -----
 
 def ImageButton(title, key):
 	return sui.Button(title, border_width=0, key=key)
@@ -97,19 +103,65 @@ def mediaPlayer(vis):
 
 	window.close()
 
+# ----- Open3D -----
+
+def setup_depth_streaming():
+	# Change file directory 
+	print("Old directory:", os.getcwd())
+	os.chdir(path)
+	print("New directory:", os.getcwd())
+
+def setup_point_clouds():
+	point_cloud = o3d.geometry.PointCloud()
+
+def setup_scene():
+	vis.add_geometry(point_cloud_name, point_cloud)
+	vis.reset_camera_to_default()
+	# vis.setup_camera(60, [4,2,5], [0,0,-1.5], [0,1,0])
+
+def update_point_clouds(file_path):
+	point_cloud = o3d.io.read_point_cloud(file_path)
+
+def update_scene():
+	vis.remove_geometry(point_cloud_name)
+	vis.add_geometry(point_cloud_name, point_cloud)
+
+def run_one_tick():
+	app = o3d.visualization.gui.Application.instance
+	tick_return = app.run_one_tick()
+	if tick_return:
+		vis.post_redraw()
+	return tick_return
+
 # Create point cloud window
 vis = o3d.visualization.O3DVisualizer("O3DVis", 1000, 700)
 vis.add_action("Custom Options", simpleui)
 vis.add_action("Video Player", mediaPlayer)
-vis.add_geometry("Hotel", hotel_cloud)
-# vis.add_geometry("Car", car_cloud)
-vis.reset_camera_to_default()
 
 # Show window
 app.add_window(vis)
-app.run()
 
-# --- Events --- ALL THIS WORKS!!!
+# Read each file and update frames
+try:
+	setup_depth_streaming()
+	setup_point_clouds()
+	setup_scene()
+
+	for file in os.listdir():
+		if file.endswith(".pcd"):
+			# FILE WONT OPEN ??? -------------------------------------------------------
+			update_point_clouds(f"{path}\{file}")
+			update_scene()
+			run_one_tick()
+			print("Open file", file, point_cloud)
+			break
+			time.sleep(1)
+except Exception as e:
+	print(e)
+
+# app.run()
+
+# --- Events --- 
 # 0: mouse_mode = out
 # 1: vis.show_skybox(out)
 # 2: point_size = out
